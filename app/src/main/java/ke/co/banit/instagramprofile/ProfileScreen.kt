4f9f81c872740944956f4d3c9a3fa7aee21ad387
Angatia Benson <bensonetia@gmail.com>
2024-05-22 05:50:49 -0500

    Pilot

---
+++ app/src/main/java/ke/co/banit/instagramprofile/ProfileScreen.kt
@@ -0,0 +1,14 @@
+package ke.co.banit.instagramprofile
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ProfileScreen(modifier: Modifier = Modifier) {
+    TopBar()
+}
+
+@Composable
+fun TopBar(modifier: Modifier = Modifier) {
+
+}
